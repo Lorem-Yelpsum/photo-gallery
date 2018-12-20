4cd94a23ab04aa22ed8daa5b1b571d52ace0d90e
Tyler Nguyen <tydnguyen@ucdavis.edu>
2018-12-20 15:37:19 -0800

    added a .connect() to database

--- database/db.js
+++ database/db.js
@@ -3,6 +3,14 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection(dbConfig);
 
+connection.connect((err => {
+    if (err) {
+        throw err;
+    } else {
+        console.log(`You're connected!`)
+    }
+})
+
 
 module.exports = {
     connection
