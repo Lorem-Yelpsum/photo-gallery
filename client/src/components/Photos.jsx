d2de3e0dafb310c1d169d4d0b6ec52810d6b6530
Tyler Nguyen <tydnguyen@ucdavis.edu>
2018-12-28 12:59:19 -0800

    add mouseover and leave functions

--- client/src/components/Photos.jsx
+++ client/src/components/Photos.jsx
@@ -7,16 +7,56 @@ import style from './Photos.css';
 class Photos extends React.Component{
     constructor(props){
         super(props)
-        this.state;
+        this.state = {
+            photo1: 'regular',
+            photo2: 'selected',
+            photo3: 'regular'
+        }
+        this.handleHover1 = this.handleHover1.bind(this);
+        this.handleHover2 = this.handleHover2.bind(this);
+        this.handleHover3 = this.handleHover3.bind(this);
+        this.handleLeave = this.handleLeave.bind(this);
+    }
+
+    handleHover1 () {
+        this.setState({
+            photo1: 'selected',
+            photo2: 'regular',
+            photo3: 'regular'
+        })
+    }
+
+    handleHover2 () {
+        this.setState({
+            photo1: 'regular',
+            photo2: 'selected',
+            photo3: 'regular'
+        })
+    }
+
+    handleHover3 () {
+        this.setState({
+            photo1: 'regular',
+            photo2: 'regular',
+            photo3: 'selected'
+        })
+    }
+
+    handleLeave () {
+        this.setState({
+            photo1: 'regular',
+            photo2: 'selected',
+            photo3: 'regular'  
+        })
     }
 
     render () {
         if (this.props.photo.length > 0) {
             return (
-                <div>
+                <div styleName ='photo-layout'>
                     <div styleName = 'photo1'>
                         <div>
-                            <img src={this.props.photo[0].url}></img>
+                            <img src={this.props.photo[0].url} styleName = {this.state.photo1} onMouseOver = {this.handleHover1} onMouseLeave = {this.handleLeave}></img>
                             <div styleName = 'overlay'>
                                 <img src={this.props.photo[0].userPic} styleName= 'userPic'></img>
                                 <div>
@@ -30,15 +70,21 @@ class Photos extends React.Component{
                     </div>
                     <div styleName= 'photo2'>
                         <div>
-                        <img src={this.props.photo[1].url}></img>
-                        <div styleName = 'overlay'>
-
-                        </div>
+                            <img src={this.props.photo[1].url} styleName = {this.state.photo2} onMouseOver={this.handleHover2} onMouseLeave={this.handleLeave}></img>
+                            <div styleName = 'overlay'>
+                                <img src={this.props.photo[1].userPic} styleName= 'userPic'></img>
+                                <div>
+                                    <div>{this.props.photo[1].comments}</div>
+                                    <span>by
+                                        <div>{this.props.photo[1].user}</div>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div styleName= 'photo3'>
                         <div>
-                        <Collection photos={this.props.photo.slice(2)} />
+                        <Collection photos={this.props.photo.slice(2)} over= {this.handleHover3} leave= {this.handleLeave}/>
                         <div styleName = 'collectionOverlay'></div>
                         </div>
                     </div>
