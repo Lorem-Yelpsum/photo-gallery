720a9989a288f6ee6a51338cd6e803d1fd9103bb
Tyler Nguyen <tydnguyen@ucdavis.edu>
2019-01-03 14:06:33 -0800

    made a config comment in db

--- database/db.js
+++ database/db.js
@@ -1,6 +1,13 @@
 const dbConfig = require('./config.js');
 const mysql = require('mysql');
 
+// module.exports = {
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'password',
+//     database: 'photo_gallery'
+// }
+
 const connection = mysql.createConnection(dbConfig);
 
 connection.connect((err) => {
