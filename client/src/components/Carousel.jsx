6e0d262ec9d7803dedd040654943aa3ede73a10c
Tyler Nguyen <tydnguyen@ucdavis.edu>
2019-03-07 17:16:21 -0800

    dynamically changed all footer icons to white

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -12,12 +12,17 @@ class Carousel extends React.Component {
             photos: [],
             close: 'unhovered',
             color: '#808080',
+            browse: '#999',
+            share: '#999',
+            compliment: '#999',
             elite: 0
         }
         this.previousImage=this.previousImage.bind(this);
         this.nextImage=this.nextImage.bind(this);
         this.hoverClose = this.hoverClose.bind(this);
         this.unhoverClose = this.unhoverClose.bind(this);
+        this.hoverBrowse = this.hoverBrowse.bind(this);
+        this.unhoverBrowse = this.unhoverBrowse.bind(this);
     }
 
     componentWillReceiveProps(props) {
@@ -64,12 +69,24 @@ class Carousel extends React.Component {
         })
     }
 
+    hoverBrowse (e) {
+        let change = {};
+        let name = e.target.getAttribute('name');
+        change[name] = 'white';
+        this.setState(change);
+    }
+
+    unhoverBrowse (e) {
+        let change = {};
+        let name = e.target.getAttribute('name')
+        change[name] = '#999';
+        this.setState(change);
+    }
 
     render () {
-        const {close, color, index, photos, elite} = this.state;
+        const {close, color, index, photos, elite, browse, share, compliment} = this.state;
         const {closeModal} = this.props
         const photoInfo = photos[index] || this.props.photos[this.props.index];
-        console.log(photoInfo);
         return (
             <div styleName= 'modal-background' onClick={ closeModal}>
                 <div styleName = 'modal-main'>
@@ -82,23 +99,42 @@ class Carousel extends React.Component {
                             <div styleName='media'>
                             <img src={photoInfo.url} styleName ='picture'></img>
                             <div styleName = 'picture-footer'>
+                                <span>
+                                    <div styleName = 'browse' onMouseEnter = {(e) => {this.hoverBrowse(e)}}
+                                    onMouseLeave = {(e)=> {this.unhoverBrowse(e)}} name ='browse'>
+                                        <Icon name={'18x18_grid'} fill={browse} width={18} height={18}/>
+                                        Browse all
+                                    </div>
+                                    <div>{index +1} of {photos.length}</div>
+                                    <div styleName = 'browse' onMouseEnter = {(e) => {this.hoverBrowse(e)}}
+                                    onMouseLeave = {(e)=> {this.unhoverBrowse(e)}} name ='share'>
+                                        <Icon name={'18x18_share'} fill={share} width={18} height={18}/>
+                                        Share
+                                    </div>
+                                    <div styleName = 'browse' onMouseEnter = {(e) => {this.hoverBrowse(e)}}
+                                    onMouseLeave = {(e)=> {this.unhoverBrowse(e)}} name ='compliment'>
+                                        <Icon name={'18x18_grid'} fill={compliment} width={18} height={18}/>
+                                        Compliment
+                                    </div>
+                                </span>
                             </div>
                             </div>
                         </div>
                         <div styleName = 'sidebar'>
                             <div styleName = 'user-info'>
                                 <img styleName = 'user-avatar' src={photoInfo.userPic}></img>
+                                <div styleName = 'name'>{photoInfo.user}</div>
                                 <div styleName ='user-stats'>
                                     <span>
                                         <div styleName='friend-count'>
                                             <span>
-                                                <Icon />
+                                                <Icon name={'18x18_friends'}  fill = {'#f15c00'}width={18} height={18}/>
                                                 <div styleName ='stat-color'>{photoInfo.friends}</div>
                                             </span>
                                         </div>
                                         <div styleName ='review-count'>
                                         <span>
-                                                <Icon />
+                                        <Icon name={'18x18_review'}  fill = {'#f15c00'}width={18} height={18}/>
                                                 <div styleName ='stat-color'>{photoInfo.reviews}</div>
                                             </span>
                                         </div>
