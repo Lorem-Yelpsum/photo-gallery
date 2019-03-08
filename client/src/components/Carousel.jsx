9a313c99e1acc00b812cfe7e325d14aa9104d189
Tyler Nguyen <tydnguyen@ucdavis.edu>
2019-03-07 21:16:13 -0800

    added everything on modal need to reposition also added a few svgs

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -15,7 +15,6 @@ class Carousel extends React.Component {
             browse: '#999',
             share: '#999',
             compliment: '#999',
-            flag: '#999',
             elite: 0
         }
         this.previousImage=this.previousImage.bind(this);
@@ -85,9 +84,10 @@ class Carousel extends React.Component {
     }
 
     render () {
-        const {close, color, index, photos, elite, browse, share, compliment, flag} = this.state;
+        const {close, color, index, photos, elite, browse, share, compliment} = this.state;
         const {closeModal} = this.props
         const photoInfo = photos[index] || this.props.photos[this.props.index];
+        console.log(photoInfo);
         return (
             <div styleName= 'modal-background' onClick={ closeModal}>
                 <div styleName = 'modal-main'>
@@ -96,57 +96,57 @@ class Carousel extends React.Component {
                     onMouseLeave={this.unhoverClose} 
                     onClick={closeModal}>
                     Close
-                    <Icon name={'24x24_close'} 
-                    fill={color} 
-                    width={24} 
-                    height={24}></Icon>
+                        <Icon name={'24x24_close'} 
+                        fill={color} 
+                        width={24} 
+                        height={24}></Icon>
                     </div>
                     <div styleName = 'picture-info'>
                         <div styleName ='picture-detail-grid'>
                             <div styleName='media'>
-                            <img src={photoInfo.url} styleName ='picture'></img>
-                            <div styleName = 'picture-footer'>
-                                <span>
-                                    <div styleName = 'browse' 
-                                    onMouseEnter = {(e) => {this.hoverBrowse(e)}}
-                                    onMouseLeave = {(e)=> {this.unhoverBrowse(e)}} 
-                                    name ='browse'>
-                                        <Icon name={'18x18_grid'} 
-                                        fill={browse} 
-                                        width={18} 
-                                        height={18}/>
-                                        Browse all
-                                    </div>
-                                    <div styleName='current-photo'>{index +1} of {photos.length}</div>
-                                    <div styleName = 'browse' 
-                                    onMouseEnter = {(e) => {this.hoverBrowse(e)}}
-                                    onMouseLeave = {(e)=> {this.unhoverBrowse(e)}} name ='share'>
-                                        <Icon name={'18x18_share'} 
-                                        fill={share} 
-                                        width={18} 
-                                        height={18}/>
-                                        Share
-                                    </div>
-                                    <div styleName = 'browse' 
-                                    onMouseEnter = {(e) => {this.hoverBrowse(e)}}
-                                    onMouseLeave = {(e)=> {this.unhoverBrowse(e)}} 
-                                    name ='compliment'>
-                                        <Icon name={'18x18_grid'} 
-                                        fill={compliment} 
-                                        width={18} 
-                                        height={18}/>
-                                        Compliment
-                                    </div>
-                                    <div name='flag'
-                                    onMouseEnter = {(e) => {this.hoverBrowse(e)}}
-                                    onMouseLeave = {(e)=> {this.unhoverBrowse(e)}}>
-                                    <Icon name={'18x18_flag'} 
-                                    fill={flag} 
-                                    width={18} 
-                                    height={18}/>
-                                    </div>
-                                </span>
-                            </div>
+                                <img src={photoInfo.url} styleName ='picture'></img>
+                                <div styleName = 'picture-footer'>
+                                    <span>
+                                        <div styleName = 'browse' 
+                                        onMouseEnter = {(e) => {this.hoverBrowse(e)}}
+                                        onMouseLeave = {(e)=> {this.unhoverBrowse(e)}} 
+                                        name ='browse'>
+                                            <Icon name={'18x18_grid'} 
+                                            fill={browse} 
+                                            width={18} 
+                                            height={18}/>
+                                            Browse all
+                                        </div>
+                                        <div styleName='current-photo'>{index +1} of {photos.length}</div>
+                                        <div styleName = 'share' 
+                                        onMouseEnter = {(e) => {this.hoverBrowse(e)}}
+                                        onMouseLeave = {(e)=> {this.unhoverBrowse(e)}} name ='share'>
+                                            <Icon name={'18x18_share'} 
+                                            fill={share} 
+                                            width={18} 
+                                            height={18}/>
+                                            Share
+                                        </div>
+                                        <div styleName = 'compliment' 
+                                        onMouseEnter = {(e) => {this.hoverBrowse(e)}}
+                                        onMouseLeave = {(e)=> {this.unhoverBrowse(e)}} 
+                                        name ='compliment'>
+                                            <Icon name={'18x18_compliment'} 
+                                            fill={compliment} 
+                                            width={18} 
+                                            height={18}/>
+                                            Compliment
+                                        </div>
+                                        <div styleName='flag'
+                                        onMouseEnter = {(e) => {this.hoverBrowse(e)}}
+                                        onMouseLeave = {(e)=> {this.unhoverBrowse(e)}}>
+                                            <Icon name={'18x18_flag'} 
+                                            fill={'#FFFFFF'} 
+                                            width={18} 
+                                            height={18}/>
+                                        </div>
+                                    </span>
+                                </div>
                             </div>
                         </div>
                         <div styleName = 'sidebar'>
@@ -180,6 +180,19 @@ class Carousel extends React.Component {
                                     </span>
                                 </div>
                             </div>
+                            <div styleName='comment'>{photoInfo.comments}</div>
+                            <div styleName='date'>{photoInfo.day}</div>
+                            <div styleName='helpful'>Was this photo ...?</div>
+                            <span>
+                                <div styleName='helpful-button'>
+                                  <Icon name={'18x18_arrow_up'} width={18} height={18} fill={'#666'}/>
+                                  Helpful
+                                </div>
+                                <div styleName='helpful-button'>
+                                <Icon name={'18x18_arrow_down'} width={18} height={18} fill={'#666'}/>
+                                  Not Helpful
+                                </div>
+                            </span>
                         </div>
                     </div>
                 </div>
